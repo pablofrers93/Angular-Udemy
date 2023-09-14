d10a5b9b94b5332005338e0b0d7d999c87508128
pablofrers93 <pablofrers93@gmail.com>
2023-09-14 15:31:29 -0300

    calculator exercise

--- calculadora-app/src/app/app.component.ts
+++ calculadora-app/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora-app';
+  
+  titulo = 'Aplicación Calculadora';
+  operandoA: number = 0;
+  operandoB: number = 0;
+  resultado: number = 0;
+
+  sumar():void{
+    this.resultado = this.operandoA+this.operandoB;
+  }
 }
